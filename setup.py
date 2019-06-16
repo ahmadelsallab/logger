95f04f30a2cfecc64a715f104e82fae89cf09a8a
ahmadelsallab <ahmad.elsallab@gmail.com>
2019-06-16 13:52:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 
 
 setup(
-   name='logger',
+   name='ml_logger',
    version='1.0',
    description='Machine learning experiments logger',
    author='Ahmad El Sallab',
